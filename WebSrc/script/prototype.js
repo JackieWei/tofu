f77f7ff3d5cd3c43ce85e38cac950667ddec2b79
Wei, Junjie <junjie.wei@163.com>
2015-01-19 00:26:35 +0800

    Initialize folders.

---
+++ WebSrc/script/prototype.js
@@ -0,0 +1,21 @@
+/**
+ * Change prototype of default objects
+ * 
+ */
+
+(function(){
+	//Object
+	
+	//String enhance
+	String.prototype.startWith = function() {
+		//TODO
+	};
+	String.prototype.endWith = function() {
+		//TODO
+	};
+	
+	//Array enhancement
+	Array.prototype.extend = function() {
+		
+	}
+})();
